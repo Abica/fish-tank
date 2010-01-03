09f65ab3dc0bc7819a57ee8ba7bf779c1260cfa2
Nicholas Wright <nick.loves.rails@gmail.com>
2010-01-03 04:53:36 -0700

    flip images correctly so everything looks proper

--- compile.rb
+++ compile.rb
@@ -1,19 +1,19 @@
-mainfile = IO.read("main.lua")
-appname = Dir.pwd[ /([^\/]+)\/?$/ ]
-builddir = "build"
-appdir = File.join( builddir, "#{ appname }Build" )
+main_file = IO.read("main.lua")
+app_name = Dir.pwd[ /([^\/]+)\/?$/ ]
+build_dir = "build"
+app_dir = File.join( build_dir, "#{ app_name }Build" )
+app_main_file = File.join( app_dir, "main.lua" ) 
 
 already_required = []
 
 puts "Merging files"
-while editing = mainfile[ /require\s?"/ ]
-  mainfile.gsub!( /(require\s?"(\w+)")/ ) do
+while editing = main_file[ /require\s?"/ ]
+  main_file.gsub!( /(require\s?"(\w+)")/ ) do
     filename =  "#{ $2 }.lua"  
 
     if File.exists?( filename )
       if already_required.include?( filename )
         ""
-
       else
         already_required << filename
         IO.read( filename )
@@ -24,31 +24,30 @@ while editing = mainfile[ /require\s?"/ ]
   end
 end
 
-unless File.exists?( builddir )
+unless File.exists?( build_dir )
   puts "Creating build/"
-  Dir.mkdir( builddir )
+  Dir.mkdir( build_dir )
 end
 
-if File.exists?( appdir )
+if File.exists?( app_dir )
   puts "Old build found, cleaning up..."
-  Dir[ File.join( appdir, "*" ) ].each do | file |
+  Dir[ File.join( app_dir, "*" ) ].each do | file |
     puts "* removing #{ file }"
     File.delete( file )
   end
 
 else
-  Dir.mkdir( appdir )
+  Dir.mkdir( app_dir )
 end
 
-appmainfile = File.join( appdir, "main.lua" ) 
-puts "* writing #{ appmainfile }"
-open( appmainfile , "w+" ) do | file |
-  file.write( mainfile )
+puts "* writing #{ app_main_file }"
+open( app_main_file , "w+" ) do | file |
+  file.write( main_file )
 end
 
 if File.exists?( "images" )
   puts "* copying images"
-  `cp images/* #{ appdir }/`
+  `cp images/* #{ app_dir }/`
 end
 
 puts "Done"
